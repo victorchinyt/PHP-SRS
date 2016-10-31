0284d9647aaa056f8eb6971ae5d0322780ea76f9
clementchuo <clementchuo@gmail.com>
2016-10-31 14:46:31 +0800

    implement export function for sales record

--- record.php
+++ record.php
@@ -97,7 +97,7 @@ function filterTable($query)
                             <input type="submit" name="delete" value="Delete" />
                         </div>
                     </div><br/>
-                    
+                    <div id="dvData">
                     <table id="salesrecord" class="table table-striped table-hover" border="5" cellpadding="5" cellspacing="0" style="border-collapse: collapse" bordercolor="#808080" width="100&#37">
                         <div class="yesPrint">
                             <tr>
@@ -138,6 +138,7 @@ function filterTable($query)
                             ?>
                         </div>
                     </table>
+                    </div>
                     
                     <!-- first row at bottom section -->
                     <div class="row">
@@ -155,6 +156,9 @@ function filterTable($query)
                         </div>
                         <div class="col-sm-2">
                             <input type="button" name="printTable" id="printTable" value="Print" onClick="printData()"/>
+                        </div>
+                         <div class='col-sm-2 button'>
+                            <button><a href="#" id ="export" role='button'>Export</a></button>
                         </div>
                     </div>
                     <br/><br/>
@@ -246,6 +250,75 @@ function filterTable($query)
             newWin.close();
         }
     </script>
+    <script type='text/javascript' src='https://code.jquery.com/jquery-1.11.0.min.js'></script>
+        <!-- If you want to use jquery 2+: https://code.jquery.com/jquery-2.1.0.min.js -->
+        <script type='text/javascript'>
+        $(document).ready(function () {
+            console.log("HELLO")
+            function exportTableToCSV($table, filename) {
+                var $headers = $table.find('tr:has(th)')
+                    ,$rows = $table.find('tr:has(td)')
+                    // Temporary delimiter characters unlikely to be typed by keyboard
+                    // This is to avoid accidentally splitting the actual contents
+                    ,tmpColDelim = String.fromCharCode(11) // vertical tab character
+                    ,tmpRowDelim = String.fromCharCode(0) // null character
+                    // actual delimiter characters for CSV format
+                    ,colDelim = '","'
+                    ,rowDelim = '"\r\n"';
+                    // Grab text from table into CSV formatted string
+                    var csv = '"';
+                    csv += formatRows($headers.map(grabRow));
+                    csv += rowDelim;
+                    csv += formatRows($rows.map(grabRow)) + '"';
+                    // Data URI
+                    var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+                $(this)
+                    .attr({
+                    'download': filename
+                        ,'href': csvData
+                        //,'target' : '_blank' //if you want it to open in a new window
+                });
+                //------------------------------------------------------------
+                // Helper Functions 
+                //------------------------------------------------------------
+                // Format the output so it has the appropriate delimiters
+                function formatRows(rows){
+                    return rows.get().join(tmpRowDelim)
+                        .split(tmpRowDelim).join(rowDelim)
+                        .split(tmpColDelim).join(colDelim);
+                }
+                // Grab and format a row from the table
+                function grabRow(i,row){
+                     
+                    var $row = $(row);
+                    //for some reason $cols = $row.find('td') || $row.find('th') won't work...
+                    var $cols = $row.find('td'); 
+                    if(!$cols.length) $cols = $row.find('th');  
+                    return $cols.map(grabCol)
+                                .get().join(tmpColDelim);
+                }
+                // Grab and format a column from the table 
+                function grabCol(j,col){
+                    var $col = $(col),
+                        $text = $col.text();
+                    return $text.replace('"', '""'); // escape double quotes
+                }
+            }
+            // This must be a hyperlink
+            $("#export").click(function (event) {
+                // var outputFile = 'export'
+                var outputFile = window.prompt("What do you want to name your output file") || 'export';
+                outputFile = outputFile.replace('.csv','') + '.csv'
+                 
+                // CSV
+                exportTableToCSV.apply(this, [$('#dvData>table'), outputFile]);
+                
+                // IF CSV, don't do event.preventDefault() or return false
+                // We actually need this to be a typical hyperlink
+            });
+        });
+    </script>
+    
 	        <!-- jQuery – required for Bootstrap plugins) --> 
             <script src="js/jquery.min.js"></script> 
             <!-- All Bootstrap  plug-ins  file --> 
